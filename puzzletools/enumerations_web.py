83f7d5b5ae395eced199072501d89dc897576f2d
Daniel R Gulotta <dgulotta@alum.mit.edu>
2016-01-15 08:39:31 -0500

    add resistor color code

--- puzzletools/enumerations_web.py
+++ puzzletools/enumerations_web.py
@@ -1,4 +1,5 @@
 from puzzletools.table_parser import parse_wikitable
+from puzzletools.morse import dash_to_hyphen
 from urllib.request import urlopen
 from time import strptime
 
@@ -10,3 +11,9 @@ def countries():
 
 def us_states():
     return download_wikitable('https://en.m.wikipedia.org/wiki/List_of_states_and_territories_of_the_United_States').make_enumeration('State',[('name',0),('abbr',1),('capital',2),('statehood',4,lambda x: strptime(x,'%B %d, %Y'))],'name')
+
+def resistor_colors():
+    t=download_wikitable('https://en.m.wikipedia.org/wiki/Electronic_color_code')
+    t.data=t.data[:12]
+    lett=lambda x : x if x.isalpha() else None
+    return t.make_enumeration('Color',[('name',0),('log_multiplier',2,lambda x: int(dash_to_hyphen(x[3:]))),('tolerance',3),('tolerance_letter',4,lett),('temp_coeff',5,lambda x: int(x) if x.isnumeric() else None),('temp_coeff_leter',6,lett)],'name')
