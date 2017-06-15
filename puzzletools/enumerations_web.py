9269bfa0ff0a89818d7a3f412253d0b88fca771e
Daniel R Gulotta <dgulotta@alum.mit.edu>
2017-06-14 23:01:26 -0400

    fix country code enumeration

--- puzzletools/enumerations_web.py
+++ puzzletools/enumerations_web.py
@@ -10,7 +10,7 @@ def download_wikitable(url,tablenum=0,fmt=None):
     return parse_wikitable(urlopen(url),tablenum,fmt)
 
 def countries():
-    return download_wikitable('https://en.m.wikipedia.org/wiki/ISO_3166-1').make_enumeration('Country',[('name',0),('alpha2',1),('alpha3',2)],'name')
+    return download_wikitable('https://en.m.wikipedia.org/wiki/ISO_3166-1',1).make_enumeration('Country',[('name',0),('alpha2',1),('alpha3',2),('numeric',3,int),('independent',5,lambda x: x=='Yes')],'name')
 
 def us_states():
     return download_wikitable('https://en.m.wikipedia.org/wiki/List_of_states_and_territories_of_the_United_States').make_enumeration('State',[('name',0),('abbr',1),('capital',2),('statehood',4,lambda x: strptime(x,'%B %d, %Y'))],'name')
